e60d53468c793efbfc1f2de5a87fd455fa4db92c
abdulrahman <abdulrahmanhamza270@gmail.com>
2025-04-04 17:32:48 +0100

    fixed the routes also

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-const {createPost, getAllPosts}  = require("../controllers/user_controller");
+const {
+    createPost,
+    getAllPosts,
+    toggleLike,
+    getFeed,
+    createComment,
+    toggleFollow,
+  }  = require("../controllers/user_controller");
 const { authenticateUser } = require("../middleware/authentication");
 
 router.post("/post", authenticateUser, createPost);
 router.get("/getallpost", authenticateUser, getAllPosts);
+router.post("/like", authenticateUser, toggleLike);
+router.get("/feed", authenticateUser, getFeed);
+router.post("/comment", authenticateUser, createComment);
+router.post("/follow", authenticateUser, toggleFollow);
 
 module.exports = router;
 
